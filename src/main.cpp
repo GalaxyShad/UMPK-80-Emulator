210f2576005c0703b8d688b488618e4a9202ed4a
GalaxyShad <vadim.kadyrin@mail.ru>
2023-08-26 19:35:04 +0500

    fix: bin loading

--- src/main.cpp
+++ src/main.cpp
@@ -5,38 +5,5 @@
 int main() {
     View app;
     app.start();
-    
-    // Controller ctr;
-
-    // ctr.onBtnStart();
-
-    // Umpk80 umpk;
-
-    // char os[0x800] = {0}; 
-
-    // std::ifstream file(OS_FILE, std::ios::binary);
-
-    // file.read(os, 0x800);
-    // file.close();
-
-    // umpk.loadOS((const uint8_t*)os);
-
-    // // umpk.getCpu().setProgramCounter(0x447);
-    // while (true) {
-    //     Cpu& cpu = umpk.getCpu();
-    //     // printf(
-    //     //     "PG=%04x CMD=%02x A=%02x H=%02x L=%02x\n", 
-    //     //     cpu.getProgramCounter(),
-    //     //     cpu.getCommandRegister(),
-    //     //     cpu.getRegister(Cpu::Register::A),
-    //     //     cpu.getRegister(Cpu::Register::H),
-    //     //     cpu.getRegister(Cpu::Register::L)
-    //     // );
-
-    //     umpk.tick();
-
-
-    // }
-
     return 0;
 }
